f596c78484c0f27f92b07108baa6c3080856e52c
DeeDeeShiba <a.davidromero@icloud.com>
2023-04-18 13:17:03 -0400

    Added network package to deal with API connection logic (currently starting out with Spotify API), Also added Moshi dependency to parse json data

---
+++ app/src/main/java/com/example/distributedmobilitygroupproject/network/SpotifyAPIService.kt
@@ -0,0 +1,29 @@
+package com.example.distributedmobilitygroupproject.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+/**
+ * Student ID: 991555778
+ * Distributed Mobility Group Project
+ * created by davidromero
+ * on 2023-04-17
+ **/
+
+
+private const val BASE_URL = "https://spotify23.p.rapidapi.com/artists/?ids=2w9zwq3AktTeYYMuhMjju8"
+
+/*
+Create Retrofit object and tell it to fetch data from webservice,
+aswell as be able to read JSON response; which is needed in RESTful API's
+*/
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface SpotifyApiService { //Fetch artist spoitfy ID given string name
+    @GET("artist")
+    fun getArtistID()
+}
